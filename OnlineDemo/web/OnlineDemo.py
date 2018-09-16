980a978741f6f18704e4aa93ff142965f5740e73
ERIKAZU <mz474@cornell.edu>
2018-09-16 11:15:13 -0700

    added simple homepage

--- OnlineDemo/web/OnlineDemo.py
+++ OnlineDemo/web/OnlineDemo.py
@@ -2,25 +2,25 @@ from flask import Flask, request, render_template, make_response, send_from_dire
 
 app = Flask(__name__)
 
+headers = {'Content-Type': 'text/html'}
 
 @app.route('/')
 def hello_world():
-    return 'You can find: \n Project [Exploration on Death and Time] at /Lifetime \n ' \
-           'Project [Mental Health] at /MentalHealth'
+    return make_response(render_template('index.html'), 200, headers)
+    # return 'You can find: \n Project [Exploration on Death and Time] at /Lifetime \n ' \
+    #        'Project [Mental Health] at /MentalHealth'
 
 @app.route('/Time')
 def death_and_time():
-    headers = {'Content-Type': 'text/html'}
+
     return make_response(render_template('death_and_time.html'), 200, headers)
 
 @app.route('/MentalHealth')
 def mental_health():
-    headers = {'Content-Type': 'text/html'}
     return make_response(render_template('mental_health.html'), 200, headers)
 
 @app.route('/HarryPotter')
 def harry_potter():
-    headers = {'Content-Type': 'text/html'}
     return make_response(render_template('harry_potter.html'), 200, headers)
 
 @app.route('/img/<path:filepath>')
